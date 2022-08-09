eaaf12f861fbd157b469291ad58d66e6ee7b5ec4
nappdevsecops <110616244+nappdevsecops@users.noreply.github.com>
2022-08-09 14:50:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
   //response.write('Hola Mundo\n')
   response.render('<h1>Hola Mundo</h1>')
+  
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
